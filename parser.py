dc85efbd6fe970efc9637faf0db7a4f4fe6d1d56
Evgenii Matveev <mfb.eugene@gmail.com>
2021-01-24 13:02:37 +0300

    first attempt to format dictionay

---
+++ parser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+
+data_formated = {}
+
+with open('./dictionary.json') as json_dict:
+    data = json.load(json_dict)
+    for key in data.keys():
+        item_array = data[key].split("ИСТОЧНИК:")
+        new_item = {
+            "english": item_array[0][item_array[0].find('(') + 1:item_array[0].find(')')],
+            "meaning": item_array[0][item_array[0].find(')') + 2:].strip(),
+            "resource": item_array[1].strip(),
+            "related": []
+        }
+        data_formated[key] = new_item
+
+with open('parser_output.json', 'w', encoding='utf8') as outfile:
+    json.dump(data_formated, outfile, ensure_ascii=False)
